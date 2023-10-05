c8518e4eb5af8922cf8fa27f7802076004a1a7d9
Asreeth <asreethp@gmail.com>
2023-10-05 08:19:06 +0530

    added Sign in Feature

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ const analytics = getAnalytics(app);
 
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 const sub = document.getElementById("Sub");
-sub.addEventListener("click",(event)=>
-
-{
+sub.addEventListener("click", (event) => {
     event.preventDefault();
     console.log('asff')
     const auth = getAuth();
@@ -54,3 +52,31 @@ sub.addEventListener("click",(event)=>
 
 
 console.log('byefghiol')
+
+
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+
+const sub1 = document.getElementById("Sub_1");
+sub1.addEventListener("click", (event) => {
+    event.preventDefault();
+    const auth = getAuth();
+    const email1 = document.getElementById('email_1').value;
+    const pass1 = document.getElementById('pass_1').value;
+    signInWithEmailAndPassword(auth, email1, pass1)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            window.location.href = 'home.html';
+
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert('nope')
+        });
+
+
+})
+
+
